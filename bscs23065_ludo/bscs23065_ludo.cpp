578d446358c79679c3f8a4d4f8f20b20c9105922
Saif <bscs23065@itu.edu.pk>
2024-04-11 12:42:43 +0500

    Add project files.

---
+++ bscs23065_ludo/bscs23065_ludo.cpp
@@ -0,0 +1,30 @@
+#include <SFML/Graphics.hpp>
+using namespace sf;
+int main() {
+    // Create a window with SFML
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Window");
+
+    // Main loop
+    while (window.isOpen()) {
+        // Handle events
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        // Clear the window
+        window.clear(sf::Color::Black);
+
+        // Draw something
+        sf::CircleShape shape(100.f);
+        shape.setFillColor(sf::Color::Green);
+        shape.setPosition(350.f, 250.f);
+        window.draw(shape);
+
+        // Display what was drawn
+        window.display();
+    }
+
+    return 0;
+}
